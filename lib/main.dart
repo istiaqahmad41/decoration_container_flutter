942ed6302ff67e4defa2d4373bb647f8083065a2
Istiaq Ahmad <istiaqahmad41@gmail.com>
2024-12-05 09:21:24 +0600

    cmt2

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-
       home: MyHomePage(),
     );
   }
@@ -28,34 +27,32 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Dashboard',style: TextStyle(color: Colors.white),),backgroundColor: Colors.deepPurpleAccent,),
-
-
-
+      appBar: AppBar(
+        title: Text(
+          'Dashboard',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.deepPurpleAccent,
+      ),
       body: Container(
         width: double.infinity,
         height: double.infinity,
-
         child: Center(
           child: Container(
             width: 100,
             height: 100,
+            //decoration part
             decoration: BoxDecoration(
               color: Colors.grey,
               //borderRadius: BorderRadius.circular(11),
               //borderRadius: BorderRadius.only(topLeft:Radius.circular(15),bottomRight: Radius.circular(15) ),
-border: Border.all(
-       width: 5,
-  color: Colors.black
-),
-                boxShadow:[ BoxShadow(
-                  blurRadius: 10,
-                  color: Colors.grey,
-                  spreadRadius: 5
-                )],
+              border: Border.all(width: 5, color: Colors.black),
+              //shadow to a object code part
+              boxShadow: [
+                BoxShadow(blurRadius: 10, color: Colors.grey, spreadRadius: 5)
+              ],
               shape: BoxShape.circle,
             ),
-
           ),
         ),
       ),
